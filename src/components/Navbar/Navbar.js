b34600f1060d8ea076403cc6e812642a35ff0a75
Brandon Vallejo <vallejobrandon4@gmail.com>
2022-02-07 00:15:07 -0300

    Agregue un navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import "./Navbar.css"
+
+const Navbar = () =>{
+    return (
+      <nav>
+          <h1>CASACAS</h1>
+          <ul>
+              <li><a href="#">Inicio</a></li>
+              <li><a href="#">Premier League</a></li>
+              <li><a href="#">LaLiga</a></li>
+              <li><a href="#">Vintage</a></li>
+          </ul>
+      </nav>
+    )
+  }
+
+  export default Navbar
