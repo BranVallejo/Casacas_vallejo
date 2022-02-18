1b87ca0658da079d3216191541c718dd3a4d8b2a
Brandon <vallejobrandon4@gmail.com>
2022-02-18 19:41:14 -0300

    cards arregladas

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,13 +4,13 @@ import "./Item.css"
 const Item = ({product}) =>{
     return(
         <div className="Card">
-            <img className="fotoProducto" src={product.img}/>
-            
             <div className="descripcionProducto">
                 <p className="nombreCamiseta">{product.name}</p>
-                <p>{product.price}</p>
-                <p>Stock disponible:{product.stock}</p>
-                <p>{product.description}</p>
+                <img className="fotoProducto" src={product.img}/>
+                <p className="product-price">${product.price}</p>
+                <p className="comprar">Agregar al carrito</p>
+                {/* <p>Stock disponible:{product.stock}</p>
+                <p>{product.description}</p> */}
             </div>
             
         </div>
--- src/components/mock/products.js
+++ src/components/mock/products.js
@@ -1,7 +1,7 @@
 export const products = [
-    {id: 1, name: "West ham Local" , price: "usd 60" , category: "Premier", img: "https://cdn.shopify.com/s/files/1/0016/1061/2806/products/ac-milan-2021-22-puma-third-kit-6_9038cbe9-ad36-4534-81c6-b2d6c05782c2_1024x1024@2x.png?v=1641423618", stock:20 , description:"Descripcion de camiseta West Ham" },
-    {id: 2, name: "Barcelona visitante", price: "usd 90", category: "LaLiga", img:"https://cdn.shopify.com/s/files/1/0016/1061/2806/products/segunda-camiseta-nike-fc-barcelona-2021-22-9_eb3d2930-58e7-4364-9384-5fb97daae505_1024x1024@2x.jpg?v=1634149483" , stock:14 , description:"Descripcion de camiseta Barcelona" },
-    {id: 3, name: "Argentina mundial 2006", price: "usd 80", category: "Vintage", img:"https://cdn.shopify.com/s/files/1/0016/1061/2806/products/argentina-titular-2006-155376_1024x1024@2x.jpg?v=1631653464" , stock:10 , description:"Descripcion de camiseta Argentina" }
+    {id: 1, name: "West ham Local" , price: "60" , category: "Premier", img: "https://cdn.shopify.com/s/files/1/0016/1061/2806/products/ac-milan-2021-22-puma-third-kit-6_9038cbe9-ad36-4534-81c6-b2d6c05782c2_1024x1024@2x.png?v=1641423618", stock:20 , description:"Descripcion de camiseta West Ham" },
+    {id: 2, name: "Barcelona visitante", price: "90", category: "LaLiga", img:"https://cdn.shopify.com/s/files/1/0016/1061/2806/products/segunda-camiseta-nike-fc-barcelona-2021-22-9_eb3d2930-58e7-4364-9384-5fb97daae505_1024x1024@2x.jpg?v=1634149483" , stock:14 , description:"Descripcion de camiseta Barcelona" },
+    {id: 3, name: "Argentina mundial 2006", price: "80", category: "Vintage", img:"https://cdn.shopify.com/s/files/1/0016/1061/2806/products/argentina-titular-2006-155376_1024x1024@2x.jpg?v=1631653464" , stock:10 , description:"Descripcion de camiseta Argentina" }
 ]
 
 
